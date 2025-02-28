d82d9cb6bd2c1e9061e4d27b67e16b19ce63b8de
Gabrielvieira1215 <126898641+Gabrielvieira1215@users.noreply.github.com>
2025-02-27 21:15:49 -0300

    alteração-JS

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-/* Formulario com erro */
 
 document
   .getElementById('formUser')
   .addEventListener('submit', function (event) {
     let isValid = true;
     const inputs = document.querySelectorAll('input');
+    let alertShown = false; // Flag para verificar se o alerta já foi exibido
 
     inputs.forEach((input) => {
-      const errorMessage = input.nextElementSibling;
+      // Verifica se o campo está vazio
       if (input.value.trim() === '') {
-        input.classList.add('error');
-        input.classList.remove('valid');
-        errorMessage.style.display = 'block';
         isValid = false;
-        alert("Por favor, insira o seu nome!");
-      } else {
-        input.classList.add('valid');
-        input.classList.remove('error');
-        errorMessage.style.display = 'none';
-        alert("Por favor, insira o seu e-mail");
+
+        // Exibir alerta apenas se ainda não tiver sido exibido
+        if (!alertShown) {
+          if (input.id === 'nome') {
+            alert("Por favor, insira o seu nome!");
+          } else if (input.id === 'email') {
+            alert("Por favor, insira o seu e-mail!");
+          }
+          alertShown = true; // Marcar que o alerta foi exibido
+        }
       }
     });
 
+    // Impede o envio do formulário se algum campo estiver vazio
     if (!isValid) {
       event.preventDefault();
     }
   });
+
+
+// Seleciona o header
+const header = document.querySelector('header');
+
+// Função para verificar a largura da janela
+function verificarTamanho() {
+    if (window.innerWidth < 600) {
+        header.style.display = 'none'; // Esconde o header se a largura for menor que 600px
+    } else {
+        header.style.display = 'block'; // Mostra o header caso a largura seja maior ou igual a 600px
+    }
+}
+
+// Chama a função inicialmente para verificar o tamanho da janela
+verificarTamanho();
+
+// Adiciona um ouvinte de evento para ajustar o comportamento quando o tamanho da janela mudar
+window.addEventListener('resize', verificarTamanho);
